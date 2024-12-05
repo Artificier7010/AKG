9779cb2940722a469d4b5a8193b81fcba7831d98
aartificiers <artify.aartificiers@gmail.com>
2024-12-06 03:40:52 +0530

    efewfew

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/',
+  base: '/',
   plugins: [
     {
       name: 'treat-js-files-as-jsx',
       async transform(code, id) {
-        if (!id.match(/src\/.*\.js$/))  return null
+        if (!id.match(/src\/.*\.js$/)) return null
 
         // Use the exposed transform from vite, instead of directly
         // transforming with esbuild
@@ -20,6 +20,11 @@ export default defineConfig({
     },
     react(),
   ],
+  build: {
+    rollupOptions: {
+      input: './src/main.jsx', // Ensure proper entry point
+    },
+  },
 
   optimizeDeps: {
     force: true,
